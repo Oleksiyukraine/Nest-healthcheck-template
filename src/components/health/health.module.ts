5e90e11f09e236071edc5701ee953f1327b431bf
Oleksiy <oleksii.motornyi@onix-systems.com>
2021-03-17 10:57:51 +0200

    fix

--- src/components/health/health.module.ts
+++ src/components/health/health.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
     imports: [
         TerminusModule,
-        MongooseModule.forRoot('mongodb+srv://test:PjXjuvi6pSRud5k@cluster0.qqsty.mongodb.net/test?retryWrites=true&w=majority'),
+        MongooseModule.forRoot(process.env.MONGO_URI),
     ],
     controllers: [HealthController],
 })
